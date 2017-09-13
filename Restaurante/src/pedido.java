8c0ad7808276178e065600a158de29b2b634f9ca
Jonathan_Antunes <jonathanantunes@hotmail.com>
2017-09-13 15:49:02 -0300

    "Criada classe Pedido."

---
+++ Restaurante/src/pedido.java
@@ -0,0 +1,14 @@
+
+public class pedido {
+ 
+	private String pedido;
+	private double precoTotal;
+	
+	public void Pedido (){
+		
+	}
+	
+	public void valorTotal (double preco1, double preco2, double preco3){
+		precoTotal = preco1 + preco2+ preco3;
+	}
+}
