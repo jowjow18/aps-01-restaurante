d29d446de97c5f67c50b7c64e0787f2f383a46f0
Jonathan_Antunes <jonathanantunes@hotmail.com>
2017-09-13 15:52:36 -0300

    "Modificado classe SistemadePedidos."

--- Restaurante/src/sistemaDePedidos.java
+++ Restaurante/src/sistemaDePedidos.java
@@ -5,8 +5,11 @@ public class sistemaDePedidos {
 		// TODO Auto-generated method stub
 
 		restaurante r = new restaurante();
+		double total = 0;
 		
 		r.setmostrarCardapio();
+		
+		System.out.println("Valor total: " + total);
 	}
 
 }
